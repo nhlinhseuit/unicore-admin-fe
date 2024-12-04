4fae477e15d4883c9f589d77aa064938f425ba4c
nhlinhizota <93470801+nhlinhseuit@users.noreply.github.com>
2024-12-04 14:36:45 +0700

    round score setting

--- app/(root)/setting/(page)/page.tsx
+++ app/(root)/setting/(page)/page.tsx
@@ -1,35 +1,74 @@
 "use client";
-
-import IconButton from "@/components/shared/Button/IconButton";
-import { mockCategoryList, mockNotiTypeList } from "@/mocks";
+import BorderContainer from "@/components/shared/BorderContainer";
+import ToggleTitle from "@/components/shared/ToggleTitle";
 import { useState } from "react";
 
+import SubmitButton from "@/components/shared/Button/SubmitButton";
+import { Input } from "@/components/ui/input";
+
 const Setting = () => {
-  const [selectedCategories, setSelectedCategories] = useState<number[]>(
-    mockCategoryList.map((item) => item.id)
+  const [isToggleShowCourseSetting, setIsToggleShowCourseSetting] =
+    useState(true);
+  const [scoreCourse, setScoreCourse] = useState("0.5");
+  const [scoreAverage, setScoreAverage] = useState("0.1");
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <ToggleTitle
+          text="Cài đặt lớp"
+          handleClick={() => {
+            setIsToggleShowCourseSetting(!isToggleShowCourseSetting);
+          }}
+          value={isToggleShowCourseSetting}
+        />
+        {isToggleShowCourseSetting ? (
+          <div className="mx-6 pl-6 flex flex-col gap-6">
+            <div>
+              <label className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 dark:text-red-900 text-dark400_light800 text-[14px] font-medium leading-[20.8px]">
+                1. Quy tắc làm tròn số
+              </label>
+              <BorderContainer otherClasses="p-6 mt-4 flex flex-col gap-4">
+                <div className="flex gap-4 items-center">
+                  <label className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 dark:text-red-900 text-dark400_light800 text-[14px] font-medium leading-[20.8px]">
+                    - Điểm lớp làm tròn lên:
+                  </label>
+                  <div>
+                    <Input
+                      value={scoreCourse}
+                      onChange={(e) => {
+                        setScoreCourse(e.target.value);
+                      }}
+                      placeholder="Nhập điểm..."
+                      className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[40px] border"
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-4 items-center">
+                  <label className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 dark:text-red-900 text-dark400_light800 text-[14px] font-medium leading-[20.8px]">
+                    - Điểm trung bình làm tròn lên:
+                  </label>
+                  <div>
+                    <Input
+                      value={scoreAverage}
+                      onChange={(e) => {
+                        setScoreAverage(e.target.value);
+                      }}
+                      placeholder="Nhập điểm..."
+                      className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[40px] border"
+                    />
+                  </div>
+                </div>
+              </BorderContainer>
+            </div>
+
+            <div>
+              <SubmitButton text="Lưu cài đặt lớp" />
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
   );
-  const [selectedNotiTypes, setSelectedNotiTypes] = useState<number[]>(
-    mockNotiTypeList.map((item) => item.id)
-  );
-
-  const handleCategoryChange = (id: number) => {
-    setSelectedCategories((prev) =>
-      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
-    );
-  };
-
-  const handleNotiTypeChange = (id: number) => {
-    setSelectedNotiTypes((prev) =>
-      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
-    );
-  };
-
-  const handleSave = () => {
-    console.log("Selected Categories:", selectedCategories);
-    console.log("Selected Notification Types:", selectedNotiTypes);
-  };
-
-  return <div>Setting</div>;
 };
-
 export default Setting;
