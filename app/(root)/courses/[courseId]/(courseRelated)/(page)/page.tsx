1602977bccf20702f2517497a873bb247ef714c6
Nguyễn Hoàng Linh <93470801+nhlinhseuit@users.noreply.github.com>
2025-01-10 23:57:38 +0700

    fix

--- app/(root)/courses/[courseId]/(courseRelated)/(page)/page.tsx
+++ app/(root)/courses/[courseId]/(courseRelated)/(page)/page.tsx
@@ -5,26 +5,25 @@ import ExercisePostItem from "@/components/shared/PostItem/ExercisePostItem";
 import PostItem from "@/components/shared/PostItem/PostItem";
 import ReportPostItem from "@/components/shared/PostItem/ReportPostItem";
 import TableSearch from "@/components/shared/Search/TableSearch";
-import {
-  AnnouncementTypesNotRegularCourse,
-  FilterType
-} from "@/constants";
+import { AnnouncementTypesNotRegularCourse, FilterType } from "@/constants";
 import { mockPostDataCourseIdPage } from "@/mocks";
 import { Dropdown } from "flowbite-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { sClassCode, sClassId } from "../../../(courses)/(store)/courseStore";
+import Image from "next/image";
+import { useState } from "react";
 
 const page = () => {
   const pathName = usePathname();
 
   const classCode = sClassCode.use();
   const classId = sClassId.use();
-  
-  console.log('classCode', classCode) 
-  console.log('classId', classId) 
 
-  var typeFilter = FilterType.SortNewer;
+  console.log("classCode", classCode);
+  console.log("classId", classId);
+
+  const [typeFilter, setTypeFilter] = useState(FilterType.None);
 
   //! CALL API để xem course này có phải có type là internCourse hay thesisCourse hay không
   const isNotRegularCourse = true;
@@ -80,6 +79,18 @@ const page = () => {
     }
   };
 
+  const cancelDetailFilter = () => {
+    setTypeFilter(FilterType.None);
+  };
+
+  const annoucementTypes = [
+    { id: 1, value: "Thông báo" },
+    { id: 2, value: "Bài tập" },
+    { id: 3, value: "Báo cáo" },
+  ];
+
+  const [selectedAnnoucementType, setSelectedAnnoucementType] = useState(1);
+
   return (
     <div>
       <div
@@ -87,14 +98,67 @@ const page = () => {
         mt-6 mb-10 flex w-full gap-6 sm:flex-row sm:items-center justify-between"
       >
         {/* Search & Filter */}
-        <div className="flex justify-start w-1/2">
+        <div className="flex items-center gap-2 justify-start w-2/3">
           <TableSearch
             setSearchTerm={() => {}}
             searchTerm={""}
-            otherClasses="pr-2 w-[70%]"
+            otherClasses="w-[50%]"
           />
+
           <Dropdown
-            className="z-30 rounded-lg w-[30%]"
+            className="z-30 rounded-lg w-[25%]"
+            label=""
+            dismissOnClick={false}
+            renderTrigger={() => (
+              <div>
+                <IconButton
+                  text={`${
+                    annoucementTypes[selectedAnnoucementType - 1].value
+                  }`}
+                  iconRight={"/assets/icons/chevron-down.svg"}
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  border
+                />
+              </div>
+            )}
+          >
+            <div className="w-full scroll-container scroll-container-dropdown-content">
+              {annoucementTypes.map((type, index) => (
+                <Dropdown.Item
+                  key={`${type.id}_${index}`}
+                  onClick={() => {
+                    if (selectedAnnoucementType === type.id) {
+                      setSelectedAnnoucementType(1);
+                    } else {
+                      setSelectedAnnoucementType(type.id);
+                    }
+                  }}
+                  className="min-w-max"
+                >
+                  <div className="flex justify-between w-full">
+                    <p className="w-[80%] text-left line-clamp-1">
+                      {type.value}
+                    </p>
+                    {selectedAnnoucementType === type.id ? (
+                      <Image
+                        src="/assets/icons/check.svg"
+                        alt="search"
+                        width={21}
+                        height={21}
+                        className="cursor-pointer mr-2"
+                      />
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </Dropdown.Item>
+              ))}
+            </div>
+          </Dropdown>
+
+          <Dropdown
+            className="z-30 rounded-lg w-[25%]"
             label=""
             dismissOnClick={false}
             renderTrigger={() => (
@@ -117,8 +181,7 @@ const page = () => {
             <Dropdown.Header>
               <span
                 onClick={() => {
-                  // cancelDetailFilter();
-                  // handleChooseFilter(FilterType.None);
+                  cancelDetailFilter();
                 }}
                 className="block truncate text-sm font-medium cursor-pointer"
               >
@@ -144,7 +207,7 @@ const page = () => {
                   name="filterOptions"
                   value={FilterType.SortNewer}
                   onChange={() => {
-                    // handleChooseFilter(FilterType.SortNewer)
+                    setTypeFilter(FilterType.SortNewer);
                   }}
                   className="w-4 h-4  cursor-pointer bg-gray-100 border-gray-300 rounded text-primary-600"
                 />
@@ -167,14 +230,13 @@ const page = () => {
                     "
               >
                 <input
-                  // checked={typeFilter === FilterType.SortOlder}
-                  checked={true}
+                  checked={typeFilter === FilterType.SortOlder}
                   id="SortOlder"
                   type="radio"
                   name="filterOptions"
                   value={FilterType.SortOlder}
                   onChange={() => {
-                    // handleChooseFilter(FilterType.SortOlder)
+                    setTypeFilter(FilterType.SortOlder);
                   }}
                   className="w-4 h-4  cursor-pointer bg-gray-100 border-gray-300 rounded text-primary-600"
                 />
